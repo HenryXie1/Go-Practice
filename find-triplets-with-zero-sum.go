bb206866a316aa3968fd3b65049cd5d1c84f6e2e
Henry Xie <34018025+HenryXie1@users.noreply.github.com>
2019-03-21 10:21:22 +1100

    Create find-triplets-with-zero-sum.go

---
+++ find-triplets-with-zero-sum.go
@@ -0,0 +1,41 @@
+This note is to add Go implementation of the [find-triplets-with-zero-sum](https://practice.geeksforgeeks.org/problems/find-triplets-with-zero-sum/1)
+```
+package main
+
+import (
+      "fmt"
+      "sort"
+)
+
+func main () {
+ 
+      test1 := []int{0, -1, 2, -3, 1}
+      test2 := []int{1,2,3}
+      findtiplet(test1,5)
+      findtiplet(test2,3)
+}
+type myint []int
+func (a myint) Len() int { return len(a) }
+func (a myint) Swap(i,j int) { a[i],a[j] = a[j],a[i] }
+func (a myint) Less(i,j int) bool { return a[i] < a [j] }
+
+ 
+func findtiplet (t []int, n int ) int{
+
+     sort.Sort(myint(t))
+     fmt.Println(t)
+     var sum int
+     for i := 0 ; i < n -2 ; i ++ {
+          for j := i + 1; j < n -1 ; j++ {
+             if sum = t[i] + t[j] + t[j+1] ; sum == 0 {
+               fmt.Println("true")
+               return 1
+     }
+    }
+  }
+
+fmt.Println("false")   
+return 0
+ 
+}
+```
